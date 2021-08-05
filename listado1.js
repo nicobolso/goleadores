cac1ba2cbf1109af0cdd239d7bc6c75b91dc98b5
nicooterounibazo <nicolasoterounibazo@gmail.com>
2021-08-05 19:48:03 -0300

    segundo commit

---
+++ listado1.js
@@ -0,0 +1,23 @@
+const goleadores_url = "https://nicobolso.github.io/goleadores/"
+
+function cargarDatos(url) {
+    document.getElementById("data").innerHTML = "";
+    fetch(url)
+        .then(respuesta => respuesta.json())
+
+
+
+        .then(listado => {
+            let row = ""
+            for (let i = 0; i < listado.length; i++) {
+                row =
+                    `
+               <td>`+ listado[i].nombre + `</td>
+               <td>`+ listado[i].apellido + `</td>
+               <td>`+ listado[i].fechas + `</td>
+               
+               
+               `
+
+                document.getElementById("data").innerHTML += contenido
+            }
